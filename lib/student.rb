bc00392a47d7a4d4803ce028d0e5b222d16a00e1
Veronica Kim <vero.a.kim@gmail.com>
2018-04-30 23:51:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
 
-    @id = DB[:conn].execute
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
 end
