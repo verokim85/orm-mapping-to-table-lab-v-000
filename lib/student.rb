aa4b5985150c3a8e1f9ce65b94d7cd96a6fb39df
Veronica Kim <vero.a.kim@gmail.com>
2018-04-30 23:50:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,10 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql)
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute
   end
 
 end
