bf688a1039947a228b265d4a8f5e226e6ed6c9ea
Veronica Kim <vero.a.kim@gmail.com>
2018-04-30 23:34:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,12 @@
 class Student
   attr_accessor :name, :grade
- 
+
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
-    @id = id 
-  end 
+    @id = id
+  end
 
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
