be2c57493fae65212303808b32d9d24ef6e22353
Veronica Kim <vero.a.kim@gmail.com>
2018-04-30 23:41:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,14 @@ class Student
   end
 
   def self.create_table
-
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+        grade INTEGER
+      )
+      )
+    SQL
   end
   #  with DB[:conn]
 
